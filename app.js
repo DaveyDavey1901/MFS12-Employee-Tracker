12476ddadaaf678d9700d91f5b211b26464596d5
David Davies <David_Davies@mail.com>
2022-01-25 14:55:49 +0000

    added the initial options with the inquirer prompt

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
-const cTable = require("console.table")
-
+const cTable = require("console.table");
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -12,5 +11,33 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) throw err;
-    console.log('Welcome you are now connected to the Employee Database');
+  console.log("Welcome you are now connected to the Employee Database");
 });
+
+// This function i used to prompt the user to choose from a list of options.
+
+const adminOption = () => {
+  inquirer.prompt({
+    name: "Admin",
+    type: "list",
+    message: "Please select an action to perform from the Employee Database.",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View aLL employees",
+      //"View all employees by manager",
+      //"view all employees by department",
+      "Add a department",
+      "Add a role",
+      "Add a employee",
+      "Update a employee role",
+      //"Update an employee managers",
+      //"Delete a department",
+      //"Delete a role",
+      //"Delete a employee",
+      //"Department budget",
+    ],
+  });
+};
+
+adminOption();
