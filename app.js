7e7c80b151708f6d81902f31bdccdd62e845b587
David Davies <David_Davies@mail.com>
2022-01-25 15:11:12 +0000

    added switch statement for user choice and commited out bonus parts

--- app.js
+++ app.js
@@ -17,27 +17,95 @@ db.connect((err) => {
 // This function i used to prompt the user to choose from a list of options.
 
 const adminOption = () => {
-  inquirer.prompt({
-    name: "Admin",
-    type: "list",
-    message: "Please select an action to perform from the Employee Database.",
-    choices: [
-      "View all departments",
-      "View all roles",
-      "View aLL employees",
-      //"View all employees by manager",
-      //"view all employees by department",
-      "Add a department",
-      "Add a role",
-      "Add a employee",
-      "Update a employee role",
-      //"Update an employee managers",
-      //"Delete a department",
-      //"Delete a role",
-      //"Delete a employee",
-      //"Department budget",
-    ],
-  });
+  inquirer
+    .prompt({
+      name: "Admin",
+      type: "list",
+      message: "Please select an action to perform from the Employee Database.",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View aLL employees",
+        //"View all employees by manager",
+        //"view all employees by department",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update a employee role",
+        //"Update an employee managers",
+        //"Delete a department",
+        //"Delete a role",
+        //"Delete a employee",
+        //"Department budget",
+        "Exit",
+      ],
+    })
+    .then((userChoice) => {
+      switch (userChoice.Admin) {
+        case "View all departments":
+          viewAllDepartments();
+          break;
+
+        case "View all roles":
+          viewAllRoles();
+          break;
+
+        case "View all employees":
+          viewAllEmpEmployees();
+          break;
+
+        // case "View all employees by manager":
+        //   viewAllEmpbyManager();
+        //   break;
+
+        // case "View all employees by department":
+        //   viewAllEmpByDepartment();
+        //   break;
+
+        case "Add a department":
+          addDepartment();
+          break;
+
+        case "Add a role":
+          addRole();
+          break;
+
+        case "Add an employee":
+          addEmployee();
+          break;
+
+        case "Update an employee's role":
+          updateEmployeeRole();
+          break;
+
+        // case "Update an employee managers":
+        //   updateEmpManager();
+        //   break;
+
+        // case "Delete a department":
+        //   deleteDepartment();
+        //   break;
+
+        // case "Delete an role":
+        //   deleteRole();
+        //   break;
+
+        // case "Delete an employee":
+        //   deleteEmployee();
+        //   break;
+
+        // case "Department budget":
+        //   departmentBudget();
+        //   break;
+
+        case "Exit":
+          exitApp();
+          break;
+
+        default:
+          break;
+      }
+    });
 };
 
 adminOption();
