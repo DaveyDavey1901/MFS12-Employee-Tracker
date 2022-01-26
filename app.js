ea49fee6b214344e86cd07f8c2776b30872a6452
David Davies <David_Davies@mail.com>
2022-01-26 00:31:39 +0000

    added functions to add departments, roles and employees also added exit app function

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const cTable = require("console.table");
 
 const db = mysql.createConnection({
   host: "localhost",
-  user: "",
-  password: "",
+  user: "#",
+  password: "#",
   database: "employeesDB",
 });
 
@@ -16,13 +16,13 @@ db.connect((err) => {
 
 // This function i used to prompt the user to choose from a list of options.
 
-
 const adminOption = () => {
   inquirer
     .prompt({
-      name: "Admin",
       type: "list",
-      message: "Please select an action to perform from the Employee Database.",
+      message:
+        "Please select an action to perform from the Employee Database.\n",
+      name: "Admin",
       choices: [
         "View all departments",
         "View all roles",
@@ -36,9 +36,10 @@ const adminOption = () => {
         //"Update an employee managers",
         //"Delete a department",
         //"Delete a role",
-        //"Delete a employee",
+        "Delete a employee",
         //"Department budget",
         "Exit",
+
       ],
     })
     .then((userChoice) => {
@@ -112,32 +113,128 @@ const adminOption = () => {
 // View section departmetns / roles / employees
 
 function viewAllDepartments() {
-  let query = "SELECT * FROM department";
+  const query = "SELECT * FROM department";
   db.query(query, (err, res) => {
     if (err) throw err;
-    console.table("All Departments: ", res);
+    console.table(res);
     adminOption();
   });
 }
 
 function viewAllRoles() {
-  let query = "SELECT * FROM role";
+  const query = "SELECT * FROM role";
   db.query(query, (err, res) => {
     if (err) throw err;
-    console.table("All Roles: ", res);
+    console.table(res);
     adminOption();
   });
 }
 
 function viewAllEmployees() {
-  let query = "SELECT * FROM employee";
+  const query = "SELECT * FROM employee";
   db.query(query, (err, res) => {
     if (err) throw err;
-    console.table("All Roles: ", res);
+    console.table(res);
     adminOption();
   });
 }
 
-// Add department / role / employee
+// Add new department / role / employee
 
+function addDepartment() {
+  inquirer
+    .prompt({
+      type: "input",
+      message: "What is the new department name?",
+      name: "department",
+    })
+    .then((res) => {
+      const query = "REPLACE INTO department (name) VALUE (?)";
+      db.query(query, [res.department], (err, res) => {
+        if (err) throw err;
+        console.table("All Department:", res);
+        adminOption();
+      });
+    });
+}
+
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the job title you would like to add?",
+        name: "title",
+      },
+      {
+        type: "input",
+        message: "What is the salary for this position?",
+        name: "salary",
+      },
+      {
+        type: "input",
+        message: "What is the department ID for this job role?",
+        name: "department_ID",
+      },
+    ])
+    .then((res) => {
+      const query =
+        "INSERT INTO role (title, salary, department_id) VALUE(?, ?, ?)";
+      db.query(
+        query,
+        [res.title, res.salary, res.department_ID],
+        (err, res) => {
+          if (err) throw err;
+          console.table(res);
+          adminOption();
+        }
+      );
+    });
+}
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your first name?",
+        name: "first_name",
+      },
+      {
+        type: "input",
+        message: "What is your last name?",
+        name: "last_name",
+      },
+      {
+        type: "input",
+        message: "What is the role_id for this job?",
+        name: "role_id",
+      },
+      {
+        type: "input",
+        message: "What is the manager_id for this job?",
+        name: "manager_id",
+      },
+    ])
+    .then((res) => {
+      const query =
+        "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUE(?, ?, ?, ?)";
+      db.query(
+        query,
+        [res.first_name, res.last_name, res.role_id, res.manager_id],
+        (err, res) => {
+          if (err) throw err;
+          console.table(res);
+          adminOption();
+        }
+      );
+    });
+}
+
+function exitApp() {
+  db.close();
+}
+// Add update managers / employee role
+// Add delete / role / employee / department
+// Add budget for departments
 adminOption();
